2fe3111bf2bb63a5c6478873a77130323e1a8aa1
Kishan <guptakishan492@gmail.com>
2022-02-15 23:27:23 +0530

    creating apifile with data in json export and inputcmdline

---
+++ data.js
@@ -0,0 +1,7 @@
+const data = [
+    { name: 'kishan sahu', email: 'guptakishan492@gmail.com', type: "software developer" },
+    { name: 'harsh sahu', email: 'sahuharsh123@gmail.com', type: "Content-manager" },
+    {name: 'mohd haider', email: 'mohdhaider321@gmail.com', type:"frontend developer"}
+
+]
+module.exports = data;
